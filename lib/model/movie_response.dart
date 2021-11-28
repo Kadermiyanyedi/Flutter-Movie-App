d574703958b666776c285ebbd4a3bd0385bb9119
Kadermiyanyedi <kadermiyanyedi@hotmail.com>
2021-11-28 16:15:29 +0300

    Create movie and movie response model

---
+++ lib/model/movie_response.dart
@@ -0,0 +1,16 @@
+import 'package:movie_app/model/movie.dart';
+
+class MovieResponse {
+  final List<Movie> movies;
+  final String error;
+
+  MovieResponse(this.movies, this.error);
+
+  MovieResponse.fromJson(Map<String, dynamic> json):
+        movies = (json["results"] as List).map((i) => new Movie.fromJson(i)).toList(),
+        error = "";
+
+  MovieResponse.withError(String errorValue):
+        movies = <Movie>[],
+        error = errorValue;
+}
