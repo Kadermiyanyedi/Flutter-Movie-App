4d33524a0815febdc579822abb4f61d382d6e5cb
elif rumeysa aydin <elifrumeysaaydin@gmail.com>
2021-12-07 23:36:24 +0300

    genre_movies.dart added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/style/theme.dart' as Style;
+import 'package:movie_app/widgets/genres.dart';
 import 'package:movie_app/widgets/now_playing.dart';
+
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -27,7 +29,8 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: ListView(
         children: <Widget>[
-          NowPlaying()
+          NowPlaying(),
+          GenresScreen()
         ],
       ),
     );
