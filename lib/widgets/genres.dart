1e274eaa458f053a06c7fac922dbbcf1d8742e07
necipozturk <90090985+necipozturk@users.noreply.github.com>
2021-12-09 00:50:55 +0300

    Add files via upload

--- lib/widgets/genres.dart
+++ lib/widgets/genres.dart
@@ -1,23 +1,22 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:movie_app/bloc/get_genres_bloc.dart';
 import 'package:movie_app/model/genre.dart';
 import 'package:movie_app/model/genre_response.dart';
-import 'package:movie_app/widgets/genres_list.dart';
+
+import 'genres_list.dart';
 
 class GenresScreen extends StatefulWidget {
-  const GenresScreen({Key? key}) : super(key: key);
-
   @override
   _GenresScreenState createState() => _GenresScreenState();
 }
 
-class _GenresScreenState extends State<GenresScreen> {
+class _GenresScreenState extends State<GenresScreen>{
+
+
   @override
   void initState() {
     super.initState();
-    genresBloc.getGenres();
+    genresBloc..getGenres();
 
   }
 
@@ -25,14 +24,14 @@ class _GenresScreenState extends State<GenresScreen> {
   Widget build(BuildContext context) {
     return StreamBuilder<GenreResponse>(
       stream: genresBloc.subject.stream,
-      builder:(context, AsyncSnapshot<GenreResponse> snapshot){
+      builder: (context, AsyncSnapshot<GenreResponse> snapshot) {
         if (snapshot.hasData) {
-          if (snapshot.data?.error != null && snapshot.data!.error.isNotEmpty) {
-            return _buildErrorWidget(snapshot.data!.error);
+          if (snapshot.data.error != null && snapshot.data.error.length > 0) {
+            return _buildErrorWidget(snapshot.data.error);
           }
-          return _buildGenreWidget(snapshot.data as GenreResponse);
+          return _buildHomeWidget(snapshot.data);
         } else if (snapshot.hasError) {
-          return _buildErrorWidget(snapshot.error as String);
+          return _buildErrorWidget(snapshot.error);
         } else {
           return _buildLoadingWidget();
         }
@@ -49,13 +48,13 @@ class _GenresScreenState extends State<GenresScreen> {
               height: 25.0,
               width: 25.0,
               child: CircularProgressIndicator(
-                valueColor: new AlwaysStoppedAnimation<Color>(Colors.white),
+                valueColor:
+                new AlwaysStoppedAnimation<Color>(Colors.white),
                 strokeWidth: 4.0,
               ),
             )
           ],
-        )
-    );
+        ));
   }
 
   Widget _buildErrorWidget(String error) {
@@ -65,18 +64,31 @@ class _GenresScreenState extends State<GenresScreen> {
           children: [
             Text("Error occured: $error"),
           ],
-        )
-    );
+        ));
   }
 
-  Widget _buildGenreWidget(GenreResponse data) {
+  Widget _buildHomeWidget(GenreResponse data) {
     List<Genre> genres = data.genres;
-    if(genres.isEmpty) {
+    print(genres);
+    if (genres.length == 0) {
       return Container(
-        child: Text("No genre"),
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Text(
+                  "No More Movies",
+                  style: TextStyle(color: Colors.black45),
+                )
+              ],
+            )
+          ],
+        ),
       );
-    } else {
-      return GenresList(genres: genres);
-    }
+    } else
+      return GenresList(genres: genres,);
   }
 }
--- lib/widgets/now_playing.dart
+++ lib/widgets/now_playing.dart
@@ -1,40 +1,38 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:movie_app/bloc/get_now_playing_bloc.dart';
-import 'package:movie_app/model/movie_response.dart';
 import 'package:movie_app/model/movie.dart';
+import 'package:movie_app/model/movie_response.dart';
+import 'package:movie_app/style/theme.dart' as Style;
 import 'package:page_indicator/page_indicator.dart';
-import 'package:movie_app/style/theme.dart' as style;
 
 class NowPlaying extends StatefulWidget {
-  const NowPlaying({Key? key}) : super(key: key);
-
   @override
   _NowPlayingState createState() => _NowPlayingState();
 }
 
 class _NowPlayingState extends State<NowPlaying> {
+  PageController pageController =
+  PageController(viewportFraction: 1, keepPage: true);
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
-    nowPlayingMoviesBloc.getMovies();
+    nowPlayingMoviesBloc..getMovies();
   }
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<MovieResponse>(
       stream: nowPlayingMoviesBloc.subject.stream,
-      builder:(context, AsyncSnapshot<MovieResponse> snapshot){
+      builder: (context, AsyncSnapshot<MovieResponse> snapshot) {
         if (snapshot.hasData) {
-          if (snapshot.data?.error != null && snapshot.data!.error.isNotEmpty) {
-            return _buildErrorWidget(snapshot.data!.error);
+          if (snapshot.data.error != null && snapshot.data.error.length > 0) {
+            return _buildErrorWidget(snapshot.data.error);
           }
-          return _buildNowPlayingWidget(snapshot.data as MovieResponse);
+          return _buildHomeWidget(snapshot.data);
         } else if (snapshot.hasError) {
-          return _buildErrorWidget(snapshot.error as String);
+          return _buildErrorWidget(snapshot.error);
         } else {
           return _buildLoadingWidget();
         }
@@ -46,18 +44,17 @@ class _NowPlayingState extends State<NowPlaying> {
     return Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
+          children: [
             SizedBox(
               height: 25.0,
               width: 25.0,
               child: CircularProgressIndicator(
-                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                valueColor: new AlwaysStoppedAnimation<Color>(Colors.white),
                 strokeWidth: 4.0,
               ),
             )
           ],
-        )
-    );
+        ));
   }
 
   Widget _buildErrorWidget(String error) {
@@ -65,15 +62,14 @@ class _NowPlayingState extends State<NowPlaying> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Error occurred: $error"),
+            Text("Error occured: $error"),
           ],
-        )
-    );
+        ));
   }
 
-  Widget _buildNowPlayingWidget(MovieResponse data){
+  Widget _buildHomeWidget(MovieResponse data) {
     List<Movie> movies = data.movies;
-    if (movies.isEmpty) {
+    if (movies.length == 0) {
       return Container(
         width: MediaQuery.of(context).size.width,
         child: Column(
@@ -81,98 +77,105 @@ class _NowPlayingState extends State<NowPlaying> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Column(
-              children: const <Widget>[
+              children: <Widget>[
                 Text(
-                  "No Movies",
+                  "No More Movies",
+                  style: TextStyle(color: Colors.black45),
                 )
               ],
             )
           ],
         ),
       );
-    }
-    else {
+    } else
       return Container(
-        height: 220,
+        height: 220.0,
         child: PageIndicatorContainer(
           align: IndicatorAlign.bottom,
+          length: movies.take(5).length,
           indicatorSpace: 8.0,
           padding: const EdgeInsets.all(5.0),
-          indicatorColor: style.Colors.titleColor,
-          indicatorSelectorColor: style.Colors.secondColor,
-          shape: IndicatorShape.circle(size: 8.0),
+          indicatorColor: Style.Colors.titleColor,
+          indicatorSelectorColor: Style.Colors.secondColor,
+          shape: IndicatorShape.circle(size: 5.0),
           child: PageView.builder(
+            controller: pageController,
             scrollDirection: Axis.horizontal,
             itemCount: movies.take(5).length,
-            itemBuilder: (context, index){
-              return Stack(
-                children: <Widget>[
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 220.0,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.rectangle,
-                      image: DecorationImage(
-                          image: NetworkImage('https://image.tmdb.org/t/p/original/' +  movies[index].backPoster),
-                          fit: BoxFit.cover,
-                      )
-                    ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(colors: [
-                        style.Colors.mainColor.withOpacity(1.0),
-                        style.Colors.mainColor.withOpacity(0.0)
-                        ],
-                       begin: Alignment.bottomCenter,
-                       end: Alignment.topCenter,
-                       stops: const [
-                         0.0,
-                         0.9
-                       ]
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {},
+                child: Stack(
+                  children: <Widget>[
+                    Hero(
+                      tag: movies[index].id,
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 220.0,
+                        decoration: new BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          image: new DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                  "https://image.tmdb.org/t/p/original/" +
+                                      movies[index].backPoster)),
+                        ),
                       ),
                     ),
-                  ),
-                  Positioned(
-                    bottom: 0.0,
-                    top: 0.0,
-                    left: 0.0,
-                    right: 0.0,
-                    child: Icon(
-                      FontAwesomeIcons.playCircle,
-                      color: Colors.red,
-                      size: 40.0,
+                    SizedBox(
+                      height: 10.0,
                     ),
-                  ),
-                  Positioned(
-                      bottom: 30.0,
-                      child: Container(
-                        padding: const EdgeInsets.only(left: 20.0, right: 10.0),
-                        width: 300.0,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              movies[index].title,
-                              style: const TextStyle(
-                                height: 1.5,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18.0
+                    Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                            stops: [
+                              0.0,
+                              0.9
+                            ],
+                            colors: [
+                              Style.Colors.mainColor.withOpacity(1.0),
+                              Style.Colors.mainColor.withOpacity(0.0)
+                            ]),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0.0,
+                      top: 0.0,
+                      left: 0.0,
+                      right: 0.0,
+                      child: Icon(
+                        FontAwesomeIcons.playCircle,
+                        color: Style.Colors.secondColor,
+                        size: 40.0,
+                      ),
+                    ),
+                    Positioned(
+                        bottom: 30.0,
+                        child: Container(
+                          padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                          width: 250.0,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                movies[index].title,
+                                style: TextStyle(
+                                    height: 1.5,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18.0),
                               ),
-                            )
-                          ],
-                        ),
-                      )
-                  )
-                ],
+                            ],
+                          ),
+                        )),
+                  ],
+                ),
               );
-
             },
           ),
-          length: movies.take(5).length,
         ),
       );
-    }
   }
 }
