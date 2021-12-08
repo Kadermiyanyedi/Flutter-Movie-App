8fee025c6fa3ee42381c83b962dec8af0a4f3f04
necipozturk <90090985+necipozturk@users.noreply.github.com>
2021-12-09 00:55:08 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:movie_app/screens/home_screen.dart';
+
+import 'screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
