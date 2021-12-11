5567eb41b2e495c091164e1e52c0ec91d468fd30
Sulecs1 <sule.akcay3@ogr.sakarya.edu.tr>
2021-12-11 15:09:40 +0300

    added overview for movie and updated paddings

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,4 +1,6 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:movie_app/bloc/get_movie_videos_bloc.dart';
 import 'package:movie_app/model/movie.dart';
 import 'package:movie_app/model/video.dart';
@@ -77,10 +79,104 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
                             "https://image.tbdm.org/t/p/original/" + movie.backPoster
                           ))
                         ),
+                        child: Container (
+                          decoration: BoxDecoration(
+                            color: Colors.black.withOpacity(0.5)
+                          ),
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                            colors: [
+                              Colors.black.withOpacity(0.9),
+                              Colors.black.withOpacity(0.0)
+                            ]
+                          )
+                        ),
                       )
                     ],
                   ),
                 ),
+              ),
+              SliverPadding(
+                  padding: EdgeInsets.all(0.0),
+                  sliver: SliverList(delegate: SliverChildListDelegate(
+                    [
+                      Padding(
+                          padding: EdgeInsets.only(left: 10.0, top: 20.0),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                movie.rating.toString(),
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(
+                                width: 5.0,
+                              ),
+                              RatingBar(
+                                itemSize: 10.0,
+                                initialRating: movie.rating / 2,
+                                minRating: 1,
+                                direction: Axis.horizontal,
+                                allowHalfRating: true,
+                                itemCount: 5,
+                                itemPadding: EdgeInsets.symmetric(horizontal: 2.0),
+                                ratingWidget: RatingWidget(
+                                  empty: Icon(
+                                    EvaIcons.star,
+                                    color: Style.Colors.secondColor,
+                                  ),
+                                  full: Icon(
+                                    EvaIcons.star,
+                                    color: Style.Colors.secondColor,
+                                  ),
+                                  half: Icon(
+                                    EvaIcons.star,
+                                    color: Style.Colors.secondColor,
+                                  ),
+                                ),
+                                onRatingUpdate: (rating) {
+                                  print(rating);
+                                },
+                              )
+                            ],
+                          ),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.only(left: 10.0, top: 20.0),
+                          child: Text("OVERVIEW", style: TextStyle(
+                            color: Style.Colors.titleColor,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 12.0
+                          ),)
+                      ),
+                      SizedBox(
+                        height: 5.0,
+                      ),
+                      Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Text(
+                            movie.overview,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12.0,
+                                height: 1.5
+                            ),
+                          ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      )
+                    ]
+                  ),),
               )
             ],
 
