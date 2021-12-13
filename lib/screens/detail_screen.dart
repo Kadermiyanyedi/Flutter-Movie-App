f44810bfb8b89940a72a5ac4a12f66de9d6959cd
cinkoozge <cinkoozge@gmail.com>
2021-12-13 21:14:58 +0300

    changes added to detail screen

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -5,20 +5,26 @@ import 'package:movie_app/bloc/get_movie_videos_bloc.dart';
 import 'package:movie_app/model/movie.dart';
 import 'package:movie_app/model/video.dart';
 import 'package:movie_app/model/video_response.dart';
+import 'package:movie_app/screens/video_player.dart';
 import 'package:movie_app/style/theme.dart' as Style;
 import 'package:movie_app/widgets/movie_info.dart';
 import 'package:movie_app/widgets/casts.dart';
+import 'package:movie_app/widgets/similar_movies.dart';
 import 'package:sliver_fab/sliver_fab.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class MovieDetailScreen extends StatefulWidget {
   final Movie movie;
+
   MovieDetailScreen({Key key, @required this.movie}) : super(key: key);
+
   @override
   _MovieDetailScreenState createState() => _MovieDetailScreenState(movie);
 }
 
 class _MovieDetailScreenState extends State<MovieDetailScreen> {
   final Movie movie;
+
   _MovieDetailScreenState(this.movie);
 
   @override
@@ -38,76 +44,79 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
     return Scaffold(
       backgroundColor: Style.Colors.mainColor,
       body: Builder(
-        builder: (context) {
-          return SliverFab(
-            floatingPosition: FloatingPosition(right: 20.0),
-            floatingWidget: StreamBuilder<VideoResponse>(
-              stream: movieVideosBloc.subject.stream,
-              builder: (context, AsyncSnapshot<VideoResponse> snapshot) {
-                if(snapshot.hasData) {
-                  if(snapshot.data.error != null && snapshot.data.error.length > 0) {
-                    return _buildErrorWidget(snapshot.data.error);
+          builder: (context) {
+            return SliverFab(
+              floatingPosition: FloatingPosition(right: 20.0),
+              floatingWidget: StreamBuilder<VideoResponse>(
+                  stream: movieVideosBloc.subject.stream,
+                  builder: (context, AsyncSnapshot<VideoResponse> snapshot) {
+                    if (snapshot.hasData) {
+                      if (snapshot.data.error != null &&
+                          snapshot.data.error.length > 0) {
+                        return _buildErrorWidget(snapshot.data.error);
+                      }
+                      return _buildVideoWidget(snapshot.data);
+                    }
+                    else if (snapshot.hasError) {
+                      return _buildErrorWidget(snapshot.error);
+                    }
+                    else {
+                      return _buildLoadingWidget();
+                    }
                   }
-                  return _buildVideoWidget(snapshot.data);
-                }
-                else if(snapshot.hasError) {
-                  return _buildErrorWidget(snapshot.error);
-                }
-                else {
-                  return _buildLoadingWidget();
-                }
-              }
-            ),
-            expandedHeight: 200.0,
-            slivers: <Widget>[
-              SliverAppBar(
-                backgroundColor: Style.Colors.mainColor,
-                expandedHeight: 200.0,
-                pinned: true,
-                flexibleSpace: FlexibleSpaceBar(
-                  title: Text(
-                    movie.title.length > 40 ? movie.title.substring(0, 40) + "..." : movie.title,
-                    style: TextStyle(
-                      fontSize: 12.0,
-                      fontWeight: FontWeight.bold
+              ),
+              expandedHeight: 200.0,
+              slivers: <Widget>[
+                SliverAppBar(
+                  backgroundColor: Style.Colors.mainColor,
+                  expandedHeight: 200.0,
+                  pinned: true,
+                  flexibleSpace: FlexibleSpaceBar(
+                    title: Text(
+                      movie.title.length > 40 ? movie.title.substring(0, 40) +
+                          "..." : movie.title,
+                      style: TextStyle(
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.bold
+                      ),
                     ),
-                  ),
-                  background: Stack(
-                    children: <Widget>[
-                      Container(
-                        decoration: BoxDecoration(
-                          shape: BoxShape.rectangle,
-                          image: DecorationImage(image: NetworkImage(
-                            "https://image.tbdm.org/t/p/original/" + movie.backPoster
-                          ))
-                        ),
-                        child: Container (
+                    background: Stack(
+                      children: <Widget>[
+                        Container(
                           decoration: BoxDecoration(
-                            color: Colors.black.withOpacity(0.5)
+                              shape: BoxShape.rectangle,
+                              image: DecorationImage(image: NetworkImage(
+                                  "https://image.tbdm.org/t/p/original/" +
+                                      movie.backPoster
+                              ))
+                          ),
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.black.withOpacity(0.5)
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            begin: Alignment.bottomCenter,
-                            end: Alignment.topCenter,
-                            colors: [
-                              Colors.black.withOpacity(0.9),
-                              Colors.black.withOpacity(0.0)
-                            ]
-                          )
-                        ),
-                      )
-                    ],
+                        Container(
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                  begin: Alignment.bottomCenter,
+                                  end: Alignment.topCenter,
+                                  colors: [
+                                    Colors.black.withOpacity(0.9),
+                                    Colors.black.withOpacity(0.0)
+                                  ]
+                              )
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              SliverPadding(
+                SliverPadding(
                   padding: EdgeInsets.all(0.0),
                   sliver: SliverList(delegate: SliverChildListDelegate(
-                    [
-                      Padding(
+                      [
+                        Padding(
                           padding: EdgeInsets.only(left: 10.0, top: 20.0),
                           child: Row(
                             crossAxisAlignment: CrossAxisAlignment.center,
@@ -130,7 +139,8 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
                                 direction: Axis.horizontal,
                                 allowHalfRating: true,
                                 itemCount: 5,
-                                itemPadding: EdgeInsets.symmetric(horizontal: 2.0),
+                                itemPadding: EdgeInsets.symmetric(
+                                    horizontal: 2.0),
                                 ratingWidget: RatingWidget(
                                   empty: Icon(
                                     EvaIcons.star,
@@ -151,19 +161,19 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
                               )
                             ],
                           ),
-                      ),
-                      Padding(
-                          padding: EdgeInsets.only(left: 10.0, top: 20.0),
-                          child: Text("OVERVIEW", style: TextStyle(
-                            color: Style.Colors.titleColor,
-                            fontWeight: FontWeight.w500,
-                            fontSize: 12.0
-                          ),)
-                      ),
-                      SizedBox(
-                        height: 5.0,
-                      ),
-                      Padding(
+                        ),
+                        Padding(
+                            padding: EdgeInsets.only(left: 10.0, top: 20.0),
+                            child: Text("OVERVIEW", style: TextStyle(
+                                color: Style.Colors.titleColor,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 12.0
+                            ),)
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Padding(
                           padding: EdgeInsets.all(10.0),
                           child: Text(
                             movie.overview,
@@ -173,22 +183,24 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
                                 height: 1.5
                             ),
                           ),
-                      ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      MovieInfo(id: movie.id,),
-                      Casts(id: movie.id,)
-                    ]
+                        ),
+                        SizedBox(
+                          height: 10.0,
+                        ),
+                        MovieInfo(id: movie.id,),
+                        Casts(id: movie.id,),
+                        SimilarMovies(id: movie.id,)
+                      ]
                   ),),
-              )
-            ],
+                )
+              ],
 
-          );
-        }
+            );
+          }
       ),
     );
   }
+
   Widget _buildLoadingWidget() {
     return Container();
   }
@@ -208,7 +220,18 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
     return FloatingActionButton(
         backgroundColor: Style.Colors.secondColor,
         child: Icon(Icons.play_arrow),
-        onPressed: null
+        onPressed: () {
+          Navigator.push(
+              context,MaterialPageRoute(
+              builder: (context)=>VideoPlayerScreen(
+            controller: YoutubePlayerController(
+              initialVideoId: videos[0].key, flags: YoutubePlayerFlags(
+              autoPlay: true, mute: true,)
+              ,)
+            ,)
+          ),
+          );
+        }
     );
   }
 }
