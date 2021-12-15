d0ebccb36e7475f94d93dd5c844df1c5c3a60bd0
cinkoozge <cinkoozge@gmail.com>
2021-12-15 15:40:19 +0300

    theme colors changed

--- lib/style/theme.dart
+++ lib/style/theme.dart
@@ -4,8 +4,8 @@ class Colors {
 
   const Colors();
 
-  static const Color mainColor = const Color(0xFF151C26);
-  static const Color secondColor = const Color(0xFFf4C10F);
-  static const Color titleColor = const Color(0xFF5a606b);
+  static const Color mainColor = const Color(0xFF203047);
+  static const Color secondColor = const Color(0xFFf7c720);
+  static const Color titleColor = const Color(0xFF697791);
 
 }
